0d97e64cc25c1bdbf2a15766d66de9e0059310cc
Sylvere Richard <sylvere.richard@gmail.com>
2022-09-09 21:23:26 +0200

    first version

--- src/main/java/org/nowherehub/springbootissue32282/HelloController.java
+++ src/main/java/org/nowherehub/springbootissue32282/HelloController.java
@@ -1,2 +1,13 @@
-package org.nowherehub.springbootissue32282;public class HelloController {
+package org.nowherehub.springbootissue32282;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String getHello() {
+        return "hello";
+    }
 }
--- src/main/java/org/nowherehub/springbootissue32282/SpringBootIssue32282Application.java
+++ src/main/java/org/nowherehub/springbootissue32282/SpringBootIssue32282Application.java
@@ -3,11 +3,13 @@ package org.nowherehub.springbootissue32282;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringBootIssue32282Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootIssue32282Application.class, args);
     }
+
 
 }
